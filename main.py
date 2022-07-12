41d7b76930b79c71cf965835e27bd4bba113e8ef
vojtech <p.prusavojta@gmail.com>
2022-07-12 21:55:25 +0200

    Changes way to find downloads folder (Win+Mac)

--- main.py
+++ main.py
@@ -3,8 +3,52 @@ from tkinter import filedialog
 from tkmacosx import Button
 from PIL import Image, ImageTk, ImageFont, ImageDraw
 import os
-from pathlib import Path
 import time
+
+# Returns Downloads folder depending on operational system
+if os.name == 'nt':
+    import ctypes
+    from ctypes import windll, wintypes
+    from uuid import UUID
+
+    # ctypes GUID copied from MSDN sample code
+    class GUID(ctypes.Structure):
+        _fields_ = [
+            ("Data1", wintypes.DWORD),
+            ("Data2", wintypes.WORD),
+            ("Data3", wintypes.WORD),
+            ("Data4", wintypes.BYTE * 8)
+        ]
+
+        def __init__(self, uuidstr):
+            uuid = UUID(uuidstr)
+            ctypes.Structure.__init__(self)
+            self.Data1, self.Data2, self.Data3, \
+            self.Data4[0], self.Data4[1], rest = uuid.fields
+            for i in range(2, 8):
+                self.Data4[i] = rest >> (8 - i - 1) * 8 & 0xff
+
+    SHGetKnownFolderPath = windll.shell32.SHGetKnownFolderPath
+    SHGetKnownFolderPath.argtypes = [
+        ctypes.POINTER(GUID), wintypes.DWORD,
+        wintypes.HANDLE, ctypes.POINTER(ctypes.c_wchar_p)
+    ]
+
+    def _get_known_folder_path(uuidstr):
+        pathptr = ctypes.c_wchar_p()
+        guid = GUID(uuidstr)
+        if SHGetKnownFolderPath(ctypes.byref(guid), 0, 0, ctypes.byref(pathptr)):
+            raise ctypes.WinError()
+        return pathptr.value
+
+    FOLDERID_Download = '{374DE290-123F-4565-9164-39C4925E467B}'
+
+    def get_download_folder():
+        return _get_known_folder_path(FOLDERID_Download)
+else:
+    def get_download_folder():
+        home = os.path.expanduser("~")
+        return os.path.join(home, "Downloads")
 
 
 def select_file():
@@ -52,7 +96,7 @@ def add_text():
 def save():
     # Save the image with watermark in original size
     # Timestamp for creating unique name
-    image.save(fp=f'{str(os.path.join(Path.home(), "Downloads"))}/watermark_image_{time.time()}.jpg')
+    image.save(fp=f'{get_download_folder()}/watermark_image_{time.time()}.jpg')
     # Notify user of saving image
     entry.delete(0, END)
     entry.insert(END, string="Image with watermark saved to the downloads folder")
