c7b0fc9d92feaa3a5e36206a973d6aea7aee6756
vojtech <p.prusavojta@gmail.com>
2022-07-14 16:43:50 +0200

    Add font color and size selectors, change GUI design

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from tkinter import filedialog
-from tkmacosx import Button
+import customtkinter
 from PIL import Image, ImageTk, ImageFont, ImageDraw
 import os
 import time
@@ -28,11 +28,13 @@ if os.name == 'nt':
             for i in range(2, 8):
                 self.Data4[i] = rest >> (8 - i - 1) * 8 & 0xff
 
+
     SHGetKnownFolderPath = windll.shell32.SHGetKnownFolderPath
     SHGetKnownFolderPath.argtypes = [
         ctypes.POINTER(GUID), wintypes.DWORD,
         wintypes.HANDLE, ctypes.POINTER(ctypes.c_wchar_p)
     ]
+
 
     def _get_known_folder_path(uuidstr):
         pathptr = ctypes.c_wchar_p()
@@ -41,7 +43,9 @@ if os.name == 'nt':
             raise ctypes.WinError()
         return pathptr.value
 
+
     FOLDERID_Download = '{374DE290-123F-4565-9164-39C4925E467B}'
+
 
     def get_download_folder():
         return _get_known_folder_path(FOLDERID_Download)
@@ -51,23 +55,30 @@ else:
         return os.path.join(home, "Downloads")
 
 
+def select_color():
+    if radio_var.get() == "black":
+        return (0, 0, 0)
+    else:
+        return (255, 255, 255)
+
+
 def select_file():
     global resized_image, new_width, new_height, filename
     # Load image
-    filetypes = (("image files", ('.png', '.jpg', '.jpeg')), ("all files", "*.*"))
+    filetypes = (("image files", ('.jpg', '.jpeg')), ("all files", "*.*"))
     filename = filedialog.askopenfilename(title="Open a file", initialdir="/", filetypes=filetypes)
     img = Image.open(filename)
-    # Resize image to fit 600x600 canvas (not saving the resized version)
+    # Resize image to fit 500x500 canvas (not saving the resized version)
     if img.height == img.width:
-        new_height = 600
-        new_width = 600
+        new_height = 500
+        new_width = 500
     elif img.height > img.width:
-        new_height = 600
-        new_width = img.width / (img.height / 600)
+        new_height = 500
+        new_width = img.width / (img.height / 500)
     elif img.height < img.width:
-        new_height = img.height / (img.width / 600)
-        new_width = 600
-    resized_image = img.resize((int(new_width), int(new_height)), Image.LANCZOS)
+        new_height = img.height / (img.width / 500)
+        new_width = 500
+    resized_image = img.resize((int(new_width), int(new_height)), Image.Resampling.LANCZOS)
     resized_image = ImageTk.PhotoImage(resized_image)
     # Show image on canvas
     canvas.config(width=int(new_width), height=int(new_height))
@@ -81,13 +92,13 @@ def add_text():
     global watermark_image, new_width, new_height, image
     # Get text, set font, open image
     text = entry.get()
-    font = ImageFont.truetype("Arial Bold.ttf", 26)
+    font = ImageFont.truetype("Arial Bold.ttf", int(slider.get()))
     image = Image.open(filename)
     # Add watermark text
     edit_image = ImageDraw.Draw(image)
-    edit_image.text((0, image.height / 2), text, (255, 255, 255), font=font)
-    # Resize image to fit 600x600 canvas (not saving the resized version)
-    watermark_image = image.resize((int(new_width), int(new_height)), Image.LANCZOS)
+    edit_image.text((0, image.height / 2), text, fill=select_color(), font=font)
+    # Resize image to fit 500x500 canvas (not saving the resized version)
+    watermark_image = image.resize((int(new_width), int(new_height)), Image.Resampling.LANCZOS)
     watermark_image = ImageTk.PhotoImage(watermark_image)
     # Show watermark image on canvas
     canvas.create_image(new_width / 2, new_height / 2, image=watermark_image)
@@ -102,23 +113,51 @@ def save():
     entry.insert(END, string="Image with watermark saved to the downloads folder")
 
 
-window = Tk()
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("green")
+
+window = customtkinter.CTk()
 window.title("Watermark App")
-window.config(background="#808080", padx=50, pady=50)
+window.geometry("640x740")
+window.config(padx=40, pady=40)
 
-canvas = Canvas(window, width=600, height=600, highlightthickness=0, background="#C0C0C0")
-canvas.grid(column=0, row=0, columnspan=5)
+# Frame
+window.grid_columnconfigure(0, weight=1)
+window.grid_rowconfigure(0, weight=1)
+window.frame = customtkinter.CTkFrame(master=window, fg_color=None)
+window.frame.grid(row=0, column=0)
 
-entry = Entry(width=52, highlightthickness=0)
-entry.insert(END, string="Enter your © copyright ar other text to use as a watermark")
-entry.grid(column=1, row=1, columnspan=3, pady=(5, 10))
+# Canvas
+canvas = Canvas(master=window.frame, width=500, height=500, highlightthickness=0, background="#C0C0C0")
+canvas.grid(column=0, row=0, columnspan=6)
 
+# Entry
+entry = customtkinter.CTkEntry(master=window.frame, width=480)
+entry.insert(END, string="Type here your © copyright or other text to use as a watermark")
+entry.grid(column=0, row=1, columnspan=6, pady=(15, 0))
 
-upload_button = Button(window, text="Upload Image", bg='#77dd77', borderless=1, width=130, command=select_file)
-upload_button.grid(column=1, row=3)
-watermark_button = Button(window, text="Add Watermark", bg='#77dd77', borderless=1, width=130, command=add_text)
-watermark_button.grid(column=2, row=3)
-download_button = Button(window, text="Save Image", bg='#77dd77', borderless=1, width=130, command=save)
-download_button.grid(column=3, row=3)
+# Radio Buttons
+radio_var = StringVar()
+radiobutton_white = customtkinter.CTkRadioButton(master=window.frame, text="White text", command=select_color,
+                                                 variable=radio_var, value="white")
+radiobutton_white.grid(column=0, row=2, columnspan=3, pady=(15, 0))
+radiobutton_white.select()
+radiobutton_black = customtkinter.CTkRadioButton(master=window.frame, text="Black text ", command=select_color,
+                                                 variable=radio_var, value="black")
+radiobutton_black.grid(column=0, row=3, columnspan=3, pady=(3, 0))
+
+# Slider
+slider_label = customtkinter.CTkLabel(master=window.frame, text="Font size slider")
+slider_label.grid(column=3, row=2, columnspan=3, pady=(15, 0))
+slider = customtkinter.CTkSlider(master=window.frame, from_=1, to=300)
+slider.grid(column=3, row=3, columnspan=3)
+
+# Buttons
+upload_button = customtkinter.CTkButton(master=window.frame, text="Upload Image", width=130, command=select_file)
+upload_button.grid(column=0, row=4, columnspan=2, pady=(15, 0))
+watermark_button = customtkinter.CTkButton(master=window.frame, text="Add Watermark", width=130, command=add_text)
+watermark_button.grid(column=2, row=4, columnspan=2, pady=(15, 0))
+download_button = customtkinter.CTkButton(master=window.frame, text="Save Image", width=130, command=save)
+download_button.grid(column=4, row=4, columnspan=2, pady=(15, 0))
 
 window.mainloop()
